8836b868b3a0071d42fc7a90550a46aa05df1dbb
hugomendonca98 <hugomendonca9@gmil.com>
2020-05-16 18:07:50 -0300

    Updates

---
+++ precache-manifest.4fcb4c4fe79a8d8be428a070a31455ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ffd06ce94e3787ce6f474cf9650ace",
+    "url": "/Nations-React/index.html"
+  },
+  {
+    "revision": "fd104f045676abf00e26",
+    "url": "/Nations-React/static/js/2.f2381b34.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Nations-React/static/js/2.f2381b34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc8a79a111ed4fffb815",
+    "url": "/Nations-React/static/js/main.56e6ba8a.chunk.js"
+  },
+  {
+    "revision": "aebcac5fcb0d8807b627",
+    "url": "/Nations-React/static/js/runtime-main.93f4f389.js"
+  }
+]);
